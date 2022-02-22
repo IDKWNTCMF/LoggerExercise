e0e565e9176ad95572cceaa02947d4b92b9be3e4
IDKWNTCMF <artemilyanich@mail.ru>
2022-02-22 18:04:26 +0300

    Some minor changes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,9 @@
 import java.io.File
 
-fun main() {
-    val inputFile = "hz_all_nodes.log"
-    val outputFile = "output.txt"
+fun main(args : Array<String>) {
+    val inputFile = if (args.isNotEmpty()) args[0] else "hz_all_nodes.log"
+    val outputFile = if (args.size >= 2) args[1] else "output.txt"
+    val cnt = if (args.size >= 3) args[2].toInt() else 10
     File(outputFile).writeText("") // Clear output from previous runs
     val log = File(inputFile).readLines()
     val threadStrings = mutableMapOf<String, Int>()
@@ -12,7 +13,7 @@ fun main() {
         threadStrings[threadName] = curValue + 1
     }
     val sortedThreads = threadStrings.toList().sortedByDescending { (_, count) -> count }
-    for (i in 0..9) {
+    for (i in 0 until cnt) {
         File(outputFile).appendText("${sortedThreads[i].first} ${sortedThreads[i].second}\n")
     }
 }
